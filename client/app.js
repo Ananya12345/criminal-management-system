af3aae455f30ff95a4acd277e2aeb760ec1e2feb
Ananya12345 <43907986+Ananya12345@users.noreply.github.com>
2019-11-02 14:26:22 +0530

    Add files via upload

---
+++ client/app.js
@@ -0,0 +1,34 @@
+const loginForm = document.getElementById("login-form");
+loginForm.addEventListener("submit", e => {
+  e.preventDefault();
+  var username = document.getElementById("uname");
+  var password = document.getElementById("pass");
+  if (username.value == "" || password.value == "") {
+    alert("No blank values allowed");
+  } else {
+    // Send the reuqest to the server thorugh "/user"
+    // with query parameter User_name which contains
+    // what the user has entered.
+    fetch(`http://localhost:4000/user?User_name="${username.value}"`)
+      .then(res => res.json())
+      .then(res => {
+        var passwordEnteredByUser = password.value;
+        console.log(res[0]);
+       
+        // res[0] is an object containing the values
+        // Username , Password , Name
+        // Check if the password returned
+        // by the server matches the one entered
+        // by the user
+        if (passwordEnteredByUser == res[0].password) {
+          alert("Login successful");
+          window.location.href = "criminal.html";
+        } else {
+          alert("wrong credentials");
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+});
